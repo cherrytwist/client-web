c5b9855dc8318514fb8f7e6dcd5a859b2ac71d11
Pim Otte <pim@otte.dev>
2021-08-05 16:58:42 +0200

    Shared whiteboard

--- src/components/Challenge/NewWhiteboardModal.tsx
+++ src/components/Challenge/NewWhiteboardModal.tsx
@@ -16,35 +16,76 @@ interface NewWhiteboardParameters {
   content: 'empty' | string;
 }
 
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const VALID_KEY_CHARS = alphabet
+  .split('')
+  .concat(alphabet.split('').map(s => s.toLowerCase()))
+  .concat(['_', '-']);
+
 const NewWhiteboardModal: FC<NewWhiteboardModalProps> = ({ show, onHide, ecoverseId, challengeId }) => {
   const initialValues: NewWhiteboardParameters = {
     visibility: 'private',
     content: 'empty',
   };
 
-  const onSubmit = async _values => {
-    console.log('On submit called');
+  const generateExcalidrawId = () => {
+    var seed = new Uint8Array(10); // half size because each byte yields 2 hexchars
+    window.crypto.getRandomValues(seed);
+    return Array.from(seed, d => d.toString(16).padStart(2, '0')).join('');
+  };
+
+  const generateExcalidrawKey = () => {
+    var seed = new Uint32Array(22);
+    window.crypto.getRandomValues(seed);
+    return Array.from(seed, d => VALID_KEY_CHARS[d % 38]).join('');
+  };
+
+  const onSubmit = async values => {
+    console.log('On submit called', values);
+    let excalidrawUrlHash = '';
     // First create resource if shared
+    if (values.visibility === 'shared') {
+      const id = generateExcalidrawId();
+      const key = generateExcalidrawKey();
+      excalidrawUrlHash += '#room=' + id + ',' + key;
+
+      //TODO: Actually create reference
+    }
+
+    if (values.content === 'prefilled') {
+      excalidrawUrlHash += '#url=http%3A//localhost%3A3000/excalidraw/' + ecoverseId + '/' + challengeId;
+    }
 
     // Then redirect to whiteboard
-    window.open('https://excalidraw.com/#url=http%3A//localhost%3A3000/excalidraw/' + ecoverseId + '/' + challengeId);
+    window.open('https://excalidraw.com/' + excalidrawUrlHash);
   };
 
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
       <Modal.Header closeButton>New Whiteboard</Modal.Header>
       <Modal.Body>
-        <Formik initialValues={initialValues} onSubmit={onSubmit} validator={() => ({})}>
-          {({ isSubmitting, handleSubmit }) => (
+        <Formik initialValues={initialValues} onSubmit={onSubmit} enableReinitialize validator={() => ({})}>
+          {({ isSubmitting, handleSubmit, setFieldValue }) => (
             <Form onSubmit={handleSubmit}>
               <label htmlFor="visibility">Visibility: </label>
-              <Form.Control name="visibility" as="select">
+              <Form.Control
+                id="visibility"
+                name="visibility"
+                as="select"
+                onChange={e => setFieldValue('visibility', e.target.value)}
+              >
                 <option value="private">Personal</option>
                 <option value="shared">Shared</option>
               </Form.Control>
               <Divider />
               <label htmlFor="content">Content: </label>
-              <Form.Control name="content" as="select">
+              <Form.Control
+                id="content"
+                name="content"
+                as="select"
+                onChange={e => setFieldValue('content', e.target.value)}
+              >
                 <option value="empty">Empty</option>
                 <option value="prefilled">Pre-filled</option>
               </Form.Control>
